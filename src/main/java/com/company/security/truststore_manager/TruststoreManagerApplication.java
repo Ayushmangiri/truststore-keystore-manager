d61a88a69bd768d39ca20ba23c703105b9677715
Ayushmangiri <ayushmangiri@gmail.com>
2026-01-16 16:16:49 +0530

    Added Component

---
+++ src/main/java/com/company/security/truststore_manager/TruststoreManagerApplication.java
@@ -0,0 +1,13 @@
+package com.company.security.truststore_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruststoreManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruststoreManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/security/truststore_manager/TruststoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.security.truststore_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruststoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
